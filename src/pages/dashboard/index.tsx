79b442a7ba3e41fa0076dc931c549cd50d964a1f
Tassio-Med <tassiohfmed@gmail.como>
2023-03-06 16:43:23 -0300

    :sparkles: feat: rota dashboard privatizada, acesso possível apenas com login

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import styles from './styles.module.css';
-import Head from 'next/head';
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
+
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
@@ -9,7 +11,26 @@ export default function Dashboard() {
         <title>Meu painel de tarefas</title>
       </Head>
 
-      <h1>Meu painel de tarefas</h1>
+      <h1>Pagina Painel</h1>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  // console.log(session);
+
+  if (!session?.user) {
+    // Se nao tem usuario vamos redirecionar para  /
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
