187bdc0da6df5a220053d249cbec03658baf8afa
Tassio-Med <tassiohfmed@gmail.como>
2023-03-06 14:56:24 -0300

    :sparkles: feat: SessionProvider importado ao _app.tsx como requisição da biblioteca next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
+import '../../styles/globals.css'
 import Header from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
