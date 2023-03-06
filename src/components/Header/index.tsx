82e85d92ebd22ffb29a83992c06a0bea69fde4cf
Tassio-Med <tassiohfmed@gmail.como>
2023-03-06 15:55:56 -0300

    feat: autenticação de login pelo google implentada no botão 'Acessar'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,14 @@ import { FaThList } from 'react-icons/fa';
 
 import React from 'react'
 import styles from './styles.module.css';
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
 
+
 export default function Header() {
+
+  const { data: session, status } = useSession();
+
   return (
     <header className={styles.header}>
       <section className={styles.content}>
@@ -17,7 +22,17 @@ export default function Header() {
           </Link>
         </nav>
 
-        <button className={styles.loginButton}>Acessar</button>
+        { status === 'loading' ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button className={styles.loginButton} onClick={() => signIn("google")}>
+            Acessar
+          </button>
+        ) }
       </section>
     </header>
   )
