6cb1ad10a0bb7d64fc0e700c449f51ca2bb39a7a
Tassio-Med <tassiohfmed@gmail.como>
2023-03-06 17:05:47 -0300

    :sparkles: feat: campo 'Minhas Tarefas' criado e estilizado no dashboard.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,12 +4,14 @@ import Head from "next/head";
 
 import { getSession } from "next-auth/react";
 import { Textarea } from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 
 export default function Dashboard() {
   return (
     <div className={styles.container}>
-      
+
       <Head>
         <title>Meu painel de tarefas</title>
       </Head>
@@ -30,6 +32,26 @@ export default function Dashboard() {
               </button>
             </form>
           </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
 
