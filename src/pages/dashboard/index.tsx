86a3d56c41ab20c8f71b5b44ab10824bd8b98772
Tassio-Med <tassiohfmed@gmail.como>
2023-03-06 16:58:08 -0300

    :sparkles: feat: componente textarea importado e estilizado

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,15 +3,36 @@ import styles from "./styles.module.css";
 import Head from "next/head";
 
 import { getSession } from "next-auth/react";
+import { Textarea } from "../../components/textarea";
+
 
 export default function Dashboard() {
   return (
     <div className={styles.container}>
+      
       <Head>
         <title>Meu painel de tarefas</title>
       </Head>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
 
-      <h1>Pagina Painel</h1>
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa publica?</label>
+              </div>
+
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+
     </div>
   );
 }
