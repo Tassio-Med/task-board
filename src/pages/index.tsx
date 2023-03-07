de69b7f0d95df5cd5689c4412142bfa44f73de55
Tassio-Med <tassiohfmed@gmail.como>
2023-03-07 15:54:36 -0300

    :sparkles: feat: número de posts e comentários atulizados via 'getStaticProps'.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/home.module.css'
 
 import heroImage from '../../assets/hero.png';
 
-export default function Home() {
+import { db } from '../services/firebaseConnection';
+import { collection, getDocs } from 'firebase/firestore';
+
+interface HomeProps{
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -27,13 +36,29 @@ export default function Home() {
         </h1>
         <div className={styles.infoContext}>
           <section className={styles.box}>
-            <span>+1000 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+3000 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </div>
-  )
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const commentRef = collection(db, "comments")
+  const postRef = collection(db, "tarefas")
+  const commentSnapshot = await getDocs(commentRef)
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60,
+  }
 }
