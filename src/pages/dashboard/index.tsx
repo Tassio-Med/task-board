c89d13813df1dd5637ecd720018b74a3570f78f3
Tassio-Med <tassiohfmed@gmail.como>
2023-03-07 11:03:55 -0300

    :sparkles: feat: campo 'Minhas tarefas' recebendo os itens e os listando.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Textarea } from "../../components/textarea";
 
 import { FiShare2 } from "react-icons/fi";
@@ -8,7 +8,7 @@ import { FaTrash } from "react-icons/fa";
 
 import { db } from "../../services/firebaseConnection";
 
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, query, orderBy, where, onSnapshot } from 'firebase/firestore'
 
 import styles from "./styles.module.css";
 import Head from "next/head";
@@ -19,10 +19,47 @@ interface HomeProps {
   }
 }
 
-export default function Dashboard({ user }: HomeProps) {
-  const [ input, setInput ] = useState("")
-  const [ publicTask, setPublicTask ] = useState(false)
+interface TaskProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  tarefa: string;
+  user: string;
+}
 
+export default function Dashboard({ user }: HomeProps) {
+  const [ input, setInput ] = useState("");
+  const [ publicTask, setPublicTask ] = useState(false);
+  const [tasks, setTasks] = useState<TaskProps[]>([])
+
+  useEffect (() => {
+    async function loadTarefas() {
+      const tarefasRef = collection(db, "tarefas");
+      const q = query(
+        tarefasRef,
+        orderBy("created", "desc"),
+        where("user", "==", user?.email )
+      );
+
+      onSnapshot(q, (snapshot) => {
+        let lista = [] as TaskProps[];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            tarefa: doc.data().tarefa,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public,
+          });
+        });
+
+        setTasks(lista);
+      });
+    }
+
+    loadTarefas();
+  }, [user?.email]);
 
   function handleChangePublic(event: ChangeEvent<HTMLInputElement>){
     setPublicTask(event.target.checked);
@@ -88,21 +125,26 @@ export default function Dashboard({ user }: HomeProps) {
         <section className={styles.taskContainer}>
           <h1>Minhas tarefas</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PUBLICO</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
+          {tasks.map((item) => (
+            <article key={item.id} className={styles.task}>
+              {item.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>PUBLICO</label>
+                  <button className={styles.shareButton}>
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div>
+              )}
 
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo show demais!</p>
-              <button className={styles.trashButton}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+              <div className={styles.taskContent}>
+                <p>{item.tarefa}</p>
+                <button className={styles.trashButton}>
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
+
         </section>
       </main>
 
