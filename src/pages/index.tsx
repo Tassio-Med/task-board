dc0375654f9b042f2c6a8d8806b559df1f04093b
Tassio-Med <tassiohfmed@gmail.como>
2023-03-08 11:35:52 -0300

    :bug: fix: tentando corrigir erro de deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../../styles/home.module.css'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import styles from "../../styles/home.module.css";
+import Image from "next/image";
 
-import heroImage from '../../assets/hero.png';
+import heroImg from "../../public/assets/hero.png";
 
-import { db } from '../services/firebaseConnection';
-import { collection, getDocs } from 'firebase/firestore';
+import { db } from "../services/firebaseConnection";
 
-interface HomeProps{
+import { collection, getDocs } from "firebase/firestore";
+
+interface HomeProps {
   posts: number;
   comments: number;
 }
@@ -17,24 +18,24 @@ export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>
-          Tasks | Home
-        </title>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
+
       <main className={styles.main}>
         <div className={styles.logoContent}>
           <Image
             className={styles.hero}
-            alt="Lista de tarefas"
-            src={heroImage}
+            alt="Logo Tarefas+"
+            src={heroImg}
             priority
           />
         </div>
         <h1 className={styles.title}>
-          Seu sistema para organizar <br />
-          tarefas e estudos
+          Sistema feito para você organizar <br />
+          seus estudos e terefas
         </h1>
-        <div className={styles.infoContext}>
+
+        <div className={styles.infoContent}>
           <section className={styles.box}>
             <span>+{posts} posts</span>
           </section>
@@ -48,17 +49,17 @@ export default function Home({ posts, comments }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tarefas");
 
-  const commentRef = collection(db, "comments")
-  const postRef = collection(db, "tarefas")
-  const commentSnapshot = await getDocs(commentRef)
+  const commentSnapshot = await getDocs(commentRef);
   const postSnapshot = await getDocs(postRef);
 
   return {
     props: {
-      posts: postSnapshot || 0,
+      posts: postSnapshot.size || 0,
       comments: commentSnapshot.size || 0,
     },
-    revalidate: 60,
-  }
-}
+    revalidate: 60, // seria revalidada a cada 60 segundos
+  };
+};
